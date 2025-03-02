f51fd5ba2f1d4665320145f60f0d2f2cc5e7c7e2
codewithsudha <sudharani0649@gmail.com>
2025-03-02 17:15:43 +0530

    gateway

---
+++ IPL_CloudGateway/src/main/java/com/nt/IplCloudGatewayApplication.java
@@ -0,0 +1,11 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplCloudGatewayApplication {
+    public static void main(String[] args) {
+		SpringApplication.run(IplCloudGatewayApplication.class, args);
+	}
+}
