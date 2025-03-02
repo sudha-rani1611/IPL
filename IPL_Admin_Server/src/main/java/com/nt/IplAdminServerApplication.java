b24441a2af8161f919354d70caff44fc7356b7f8
codewithsudha <sudharani0649@gmail.com>
2025-03-02 17:08:39 +0530

    Admin Server

---
+++ IPL_Admin_Server/src/main/java/com/nt/IplAdminServerApplication.java
@@ -0,0 +1,18 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.extern.slf4j.Slf4j;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication
+@Slf4j
+public class IplAdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplAdminServerApplication.class, args);
+	}
+
+}
